d2627e9aa4d99fd2dba636d3567d3bbd679c2ee8
conaitis1 <charles.onaitis@gmail.com>
2025-06-17 11:56:46 -0700

    r4thrthrth

--- app.py
+++ app.py
@@ -280,7 +280,8 @@ if not filtered_df.empty:
         labels = outcome_counts.index.tolist()
         sizes = outcome_counts.values.tolist()
 
-        fig, ax = plt.subplots()
+        fig, ax = plt.subplots(figsize=(3, 3))  # You can try (2.5, 2.5) or (3.5, 3.5) as well
+
         ax.pie(
             sizes,
             labels=labels,
