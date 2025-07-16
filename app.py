bb9a4bf44bfa059256586a62417f1a6258f4f2a8
conaitis1 <charles.onaitis@gmail.com>
2025-07-16 11:38:12 -0700

    3eg34344t3

--- app.py
+++ app.py
@@ -436,6 +436,7 @@ with st.form("prediction_form"):
     for i, feature in enumerate(numerical_features):
         if feature in df.columns:
             col = col1 if i % 2 == 0 else col2
+            cleaned_series = pd.to_numeric(df[feature], errors="coerce")
             min_val = float(df[feature].min())
             max_val = float(df[feature].max())
             default_val = float(df[feature].median())
